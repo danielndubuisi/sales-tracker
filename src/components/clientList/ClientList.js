f2df96b4897eeb95bdda4b7631f788fe374ec42f
Daniel Ndubuisi <ndubeansdaniel97@gmail.com>
2023-10-26 16:06:18 +0100

    update client list

--- src/components/clientList/ClientList.js
+++ src/components/clientList/ClientList.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./ClientList.scss";
 import ArrowUpwardOutlinedIcon from "@mui/icons-material/ArrowUpwardOutlined";
 import ArrowDownwardOutlinedIcon from "@mui/icons-material/ArrowDownwardOutlined";
@@ -7,11 +10,24 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableFooter from "@mui/material/TableFooter";
 import Paper from "@mui/material/Paper";
-import { useNavigate } from "react-router-dom";
+import TablePagination from "@mui/material/TablePagination";
 
 const ClientList = ({ rows }) => {
   const navigate = useNavigate();
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <TableContainer component={Paper} className="table">
@@ -30,7 +46,10 @@ const ClientList = ({ rows }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {(rowsPerPage > 0
+            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
             <TableRow
               key={row.id}
               className="tableRow"
@@ -57,6 +76,23 @@ const ClientList = ({ rows }) => {
             </TableRow>
           ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { 'aria-label': 'rows per page' },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
--- src/components/single/SingleClient.js
+++ src/components/single/SingleClient.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
-import Navbar from "../navbar/Navbar";
 import Sidebar from "../sidebar/Sidebar";
+import Widget from "../widget/Widget";
 import "./SingleClient.scss";
 import { useFetch } from "../../hooks/useFetch";
 import Spinner from "../../components/spinner/Spinner";
@@ -14,27 +14,47 @@ const SingleClient = () => {
     <div className="client">
       <Sidebar />
       <div className="client-container">
-        <Navbar />
+        <div className="client-top">
+          <h4>Client sale dashboard</h4>
+          <select name="date_select" id="date_select">
+            <option value="week">Week</option>
+            <option value="month">Month</option>
+            <option value="year">Year</option>
+          </select>
+          <span></span>
+        </div>
         {error && <p className="error">{error}</p>}
         {isPending && <Spinner />}
         {data && (
-          <div className="single">
-            <img src={data.img} alt="client-pic" />
-            <h2>Name: {data.name}</h2>
-            <p>Location: {data.lcda}</p>
-            <p>Avg Purchase: {data.avgPurchase}</p>
-            <p>Status: {data.status}</p>
-            <p>Category: {data.category}</p>
-            <p>Type: {data.clientType}</p>
-            <p>Current fulfilment(%): {data.fulfilment}</p>
-            <h4>Suggestions to show...</h4>
-            <ul>
-              <li>Purchase pattern (trend line)</li>
-              <li>Client category</li>
-              <li>Recievable days</li>
-              <li>Payment status</li>
-              <li>No. of orders</li>
-            </ul>
+          <div className="single-container">       
+            <div className="single-left">
+              <div className="widgets">
+                <Widget type="orders" />
+                <Widget type="projections" />
+                <Widget type="target" />
+              </div>
+              <div className="content">
+                <img src={data.img} alt="client-pic" />
+                <h3>{data.name}</h3>
+                <p>Location: {data.lcda}</p>
+                <p>Avg Purchase: {data.avgPurchase}</p>
+                <p>Status: {data.status}</p>
+                <p>Category: {data.category}</p>
+                <p>Type: {data.clientType}</p>
+                <p>Current fulfilment(%): {data.fulfilment}</p>
+                <h4>Suggestions to show...</h4>
+                <ul>
+                  <li>Purchase pattern based on live data (trend line)</li>
+                  <li>Client category</li>
+                  <li>Recievable days</li>
+                  <li>Payment status</li>
+                  <li>No. of orders</li>
+                </ul>
+              </div>
+            </div>
+            <div className="single-right">
+              <span></span>
+            </div>
           </div>
         )}
       </div>
